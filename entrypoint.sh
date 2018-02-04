3f1a1db1ff98f4d661f8726308e0a98d5bd7cdda
vinyll <vincent.agnano@scopyleft.fr>
2018-02-04 10:48:27 -0600

    removed ssltunel that needs prior installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ chmod -R 0770 /data
 chmod -R 777 config
 ./occ maintenance:install --admin-user=admin --admin-pass=admin --data-dir=/data
 ./occ maintenance:repair
-php -S 0.0.0.0:8080 &
-stunnel3 -d 8443 -r 8080
+php -S 0.0.0.0:8080
+#stunnel3 -d 8443 -r 8080
