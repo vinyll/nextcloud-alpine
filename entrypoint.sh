f13880d6edd45196b342eb47cc38d94e7931a216
vinyll <vincent.agnano@scopyleft.fr>
2018-02-04 00:22:07 -0600

    make config dir volume writable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 chmod -R 0770 /data
+chmod -R 777 config
 ./occ maintenance:install --admin-user=admin --admin-pass=admin --data-dir=/data
 ./occ maintenance:repair
 php -S 0.0.0.0:8080
